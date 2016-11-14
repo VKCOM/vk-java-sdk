0ec8f6303968d688e9f7c50ed62c50bbb03832d4
Anton Tsivarev <tsivarev.a@gmail.com>
2016-11-14 15:25:22 +0300

    Fix ItemNote.

--- sdk/src/main/java/com/vk/api/sdk/objects/newsfeed/ItemNote.java
+++ sdk/src/main/java/com/vk/api/sdk/objects/newsfeed/ItemNote.java
@@ -1,24 +1,27 @@
 package com.vk.api.sdk.objects.newsfeed;
 
-import com.google.gson.JsonObject;
 import com.google.gson.annotations.SerializedName;
 
+import java.util.List;
 import java.util.Objects;
 
 /**
  * ItemNote object
  */
 public class ItemNote {
-    @SerializedName("notes")
-    private JsonObject notes;
 
-    public JsonObject getNotes() {
-        return notes;
+    @SerializedName("count")
+    private Integer count;
+
+    @SerializedName("items")
+    private List<NewsfeedNote> items;
+
+    public Integer getCount() {
+        return count;
     }
 
-    @Override
-    public int hashCode() {
-        return Objects.hash(notes);
+    public List<NewsfeedNote> getItems() {
+        return items;
     }
 
     @Override
@@ -26,13 +29,20 @@ public class ItemNote {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         ItemNote itemNote = (ItemNote) o;
-        return Objects.equals(notes, itemNote.notes);
+        return Objects.equals(count, itemNote.count) &&
+                Objects.equals(items, itemNote.items);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(count, items);
     }
 
     @Override
     public String toString() {
         final StringBuilder sb = new StringBuilder("ItemNote{");
-        sb.append("notes=").append(notes);
+        sb.append("count=").append(count);
+        sb.append(", items=").append(items);
         sb.append('}');
         return sb.toString();
     }
