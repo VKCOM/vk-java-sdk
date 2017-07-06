4e47e62a36e22840c5924cc4f5624d8da434446d
Anton Tsivarev <tsivarev.a@gmail.com>
2017-07-06 09:56:02 +0300

    Change version from 0.5.5 to 0.5.6-SNAPSHOT

--- sdk/src/main/java/com/vk/api/sdk/httpclient/HttpTransportClient.java
+++ sdk/src/main/java/com/vk/api/sdk/httpclient/HttpTransportClient.java
@@ -40,7 +40,7 @@ public class HttpTransportClient implements TransportClient {
     private static final String ENCODING = "UTF-8";
     private static final String FORM_CONTENT_TYPE = "application/x-www-form-urlencoded";
     private static final String CONTENT_TYPE_HEADER = "Content-Type";
-    private static final String USER_AGENT = "Java VK SDK/0.5.5";
+    private static final String USER_AGENT = "Java VK SDK/0.5.6";
 
     private static final int MAX_SIMULTANEOUS_CONNECTIONS = 300;
     private static final int DEFAULT_RETRY_ATTEMPTS_NETWORK_ERROR_COUNT = 3;
