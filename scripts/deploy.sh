f712a88999708ed782cc1198abab2b73195b676b
Ilya Egorov <Mobyman@users.noreply.github.com>
2019-05-19 13:08:43 +0300

    Add travis (#144)

---
+++ scripts/deploy.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+if [ "$TRAVIS_BRANCH" = 'master' ]; then
+  openssl aes-256-cbc -K $encrypted_5c22135b7a45_key -iv $encrypted_5c22135b7a45_iv -in travis.gpg.enc -out travis.gpg -d
+  gradle uploadArchives -PossrhUsername=${SONATYPE_USERNAME} -PossrhPassword=${SONATYPE_PASSWORD} -Psigning.keyId=${GPG_KEY_ID} -Psigning.password=${GPG_KEY_PASSPHRASE} -Psigning.secretKeyRingFile=travis.gpg
+fi
+
