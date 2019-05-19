662f6d85875bdc43578e44634946126f90e8ed83
root <root@s2.mobyman.org>
2019-05-19 10:11:52 +0000

    Add travis

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-if [ "$TRAVIS_BRANCH" = 'master' ]; then
+if [ "$TRAVIS_BRANCH" = 'master' -a "$TRAVIS_PULL_REQUEST" = "false" ]; then
   openssl aes-256-cbc -K $encrypted_5c22135b7a45_key -iv $encrypted_5c22135b7a45_iv -in travis.gpg.enc -out travis.gpg -d
   gradle uploadArchives -PossrhUsername=${SONATYPE_USERNAME} -PossrhPassword=${SONATYPE_PASSWORD} -Psigning.keyId=${GPG_KEY_ID} -Psigning.password=${GPG_KEY_PASSPHRASE} -Psigning.secretKeyRingFile=travis.gpg
 fi
