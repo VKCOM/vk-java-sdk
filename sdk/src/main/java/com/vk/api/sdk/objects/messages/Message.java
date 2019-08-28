73a324abb20b2eb5f215f06b7a86b019fff5fd4d
Arthur Unity <apploidyakutsk@gmail.com>
2019-08-28 23:59:04 +0900

    Message POJO serialized name renamed to body from text

--- sdk/src/main/java/com/vk/api/sdk/objects/messages/Message.java
+++ sdk/src/main/java/com/vk/api/sdk/objects/messages/Message.java
@@ -118,7 +118,7 @@ public class Message implements Validable {
     /**
      * Message text
      */
-    @SerializedName("text")
+    @SerializedName("body")
     @Required
     private String text;
 
