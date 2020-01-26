a10b3efe78f17ec80718d641dcde28a485594317
malyshgit <vm.malysh@gmail.com>
2020-01-26 13:56:15 +0500

    fix templates

--- sdk/src/main/java/com/vk/api/sdk/queries/messages/MessagesSendQueryWithUserIds.java
+++ sdk/src/main/java/com/vk/api/sdk/queries/messages/MessagesSendQueryWithUserIds.java
@@ -6,6 +6,7 @@ import com.vk.api.sdk.client.VkApiClient;
 import com.vk.api.sdk.client.actors.GroupActor;
 import com.vk.api.sdk.client.actors.UserActor;
 import com.vk.api.sdk.objects.messages.Keyboard;
+import com.vk.api.sdk.objects.messages.Template;
 import com.vk.api.sdk.objects.messages.responses.SendUserIdsResponse;
 import java.util.Arrays;
 import java.util.List;
@@ -204,6 +205,10 @@ public class MessagesSendQueryWithUserIds extends AbstractQueryBuilder<MessagesS
      */
     public MessagesSendQueryWithUserIds keyboard(Keyboard value) {
         return unsafeParam("keyboard", value);
+    }
+
+    public MessagesSendQueryWithUserIds template(Template value) {
+        return unsafeParam("template", value);
     }
 
     /**
