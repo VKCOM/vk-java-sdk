64c761cdfb011ae459d76e0526ca5b1cf2f1432e
Vast-Nectarine <60979398+Vast-Nectarine@users.noreply.github.com>
2020-03-29 14:38:22 +0500

    Update StreamingEventId.java
    Added topicOwnerId, topicId, topicPostId fields.

--- sdk/src/main/java/com/vk/api/sdk/streaming/objects/StreamingEventId.java
+++ sdk/src/main/java/com/vk/api/sdk/streaming/objects/StreamingEventId.java
@@ -24,6 +24,16 @@ public class StreamingEventId {
     @SerializedName("shared_post_id")
     private Integer sharedPostId;
 
+    @SerializedName("topic_owner_id")
+    private Integer topicOwnerId;
+
+    @SerializedName("topic_id")
+    private Integer topicId;
+
+    @SerializedName("topic_post_id")
+    private Integer topicPostId;
+
+
     public Integer getPostOwnerId() {
         return postOwnerId;
     }
@@ -44,6 +54,19 @@ public class StreamingEventId {
         return sharedPostId;
     }
 
+    public Integer getTopicOwnerId() {
+        return topicOwnerId;
+    }
+
+    public Integer getTopicId() {
+        return topicId;
+    }
+
+    public Integer getTopicPostId() {
+        return topicPostId;
+    }
+
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
@@ -53,12 +76,15 @@ public class StreamingEventId {
                 Objects.equals(postId, that.postId) &&
                 Objects.equals(commentId, that.commentId) &&
                 Objects.equals(sharedPostOwnerId, that.sharedPostOwnerId) &&
-                Objects.equals(sharedPostId, that.sharedPostId);
+                Objects.equals(sharedPostId, that.sharedPostId) &&
+                Objects.equals(topicOwnerId, that.topicOwnerId) &&
+                Objects.equals(topicId, that.topicId) &&
+                Objects.equals(topicPostId, that.topicPostId);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(postOwnerId, postId, commentId, sharedPostOwnerId, sharedPostId);
+        return Objects.hash(postOwnerId, postId, commentId, sharedPostOwnerId, sharedPostId, topicOwnerId, topicId, topicPostId);
     }
 
     @Override
@@ -69,6 +95,9 @@ public class StreamingEventId {
         sb.append(", commentId=").append(commentId);
         sb.append(", sharedPostOwnerId=").append(sharedPostOwnerId);
         sb.append(", sharedPostId=").append(sharedPostId);
+        sb.append(", topicOwnerId=").append(topicOwnerId);
+        sb.append(", topicId=").append(topicId);
+        sb.append(", topicPostId=").append(topicPostId);
         sb.append('}');
         return sb.toString();
     }
